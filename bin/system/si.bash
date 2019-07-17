03fb608b571df06cad2aa6167b05ed881d9530ee
Nelbren <nelbren@gmail.com>
2019-07-16 22:26:08 -0600

    simple update

--- bin/system/si.bash
+++ bin/system/si.bash
@@ -11,6 +11,7 @@
 # v0.0.7 - 2018-09-06 - nelbren@nelbren.com
 # v0.0.8 - 2018-10-15 - nelbren@nelbren.com
 # v0.0.9 - 2019-04-02 - nelbren@nelbren.com
+# v0.1.0 - 2019-07-16 - nelbren@nelbren.com
 #
 
 use() {
@@ -308,8 +309,8 @@ check_procs() {
   subsubvalue1=${subvalue2:0:1}
   subsubvalue2=${subvalue2:1}
   color_msg $STATE_INFO ${label} "" 0 1
-  [ $subvalue1 -gt 250 ] && state=$STATE_WARNING
-  [ $subvalue1 -gt 300 ] && state=$STATE_CRITICAL
+  [ $subvalue1 -gt 350 ] && state=$STATE_WARNING
+  [ $subvalue1 -gt 400 ] && state=$STATE_CRITICAL
   color_msg $state "" ${subvalue1}
   color_msg $STATE_INFO "" \/${subsubvalue1}=
   state=$STATE_OK
