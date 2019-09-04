12a85001309b43029f68469bb2136991eec460ef
Nelbren <nelbren@gmail.com>
2019-09-04 00:56:50 -0600

    simple update

--- bin/alias/set.bash
+++ bin/alias/set.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
+#
+# set.bash
+#
+# v0.0.2 - 2019-09-04 - nelbren@nelbren.com
+#
 
 base=/usr/local/npres
 alias si="$base/bin/system/si.bash"
 
-[ "$TERM" == "dumb" ] || si
+[ "$TERM" != "dumb" -a -z "$SUDO_COMMAND" ] && si
