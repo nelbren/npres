0074adb49f62c44d51dd909d340a0a0acd1ca1ab
Nelbren <nelbren@gmail.com>
2020-07-14 03:29:31 -0600

    welcome p3!

--- bin/alias/set.bash
+++ bin/alias/set.bash
@@ -3,9 +3,11 @@
 # set.bash
 #
 # v0.0.2 - 2019-09-04 - nelbren@nelbren.com
+# v0.0.3 - 2020-07-14 - nelbren@nelbren.com
 #
 
 base=/usr/local/npres
 alias si="$base/bin/system/si.bash"
+alias p3="$base/bin/system/p3.bash"
 
 [ "$TERM" != "dumb" -a -z "$SUDO_COMMAND" ] && si
